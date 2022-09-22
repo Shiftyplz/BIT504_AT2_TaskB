de067e93b3fe688d372951c6d5a952efdc5c9068
Shiftyplz <113808448+Shiftyplz@users.noreply.github.com>
2022-09-22 20:40:35 +1200

    Changed the display text on a draw
    Made changes to the text displayed on a draw, these changes are different from the changes in experiment 1 this change addes the word HERE to "Click to play again."

--- GameMain.java
+++ GameMain.java
@@ -113,7 +113,7 @@ public class GameMain extends JPanel implements MouseListener{
 			}       
 			} else if (currentState == GameState.Draw) {          
 				statusBar.setForeground(Color.RED);          
-				statusBar.setText("It's a Draw! Click to play again.");       
+				statusBar.setText("It's a Draw!Click HERE to play again");       
 			} else if (currentState == GameState.Cross_won) {          
 				statusBar.setForeground(Color.RED);          
 				statusBar.setText("'X' Won! Click to play again.");       
